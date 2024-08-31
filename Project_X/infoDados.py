0b96bfa37970d11629f760b0d1273502c4a416e5
Project Extension <projectx12121@gmail.com>
2024-08-31 20:27:00 -0300

    Delete Project_X directory

--- Project_X/infoDados.py
+++
@@ -1,8 +0,0 @@
-from ProcessingData import dadosBrutos
-from ProcessingData import dadosTratados
-
-dados = "Database/dadosBrutos.csv"
-
-# Metodos
-dadosBrutos.main(dados)
-dadosTratados.main(dados)
